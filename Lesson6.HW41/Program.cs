09d9e3414298c22cedc6de54c51c1a766d3d657f
Skycraft <mip1991@mail.ru>
2022-07-14 23:55:12 +0300

    Add project

---
+++ Lesson6.HW41/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 41
+
+Console.WriteLine("Задача 41");
+Console.Write("Введите число элементов - ");
+int M = int.Parse(Console.ReadLine());
+
+int bolshezero(int M){
+    int count = 0;
+    for(int i = 0;i<M;i++){
+        Console.Write("Введите число №" + (i+1) + " - ");
+        if (int.Parse(Console.ReadLine())>0)
+            count = count + 1;
+    }
+
+    return count;
+}
+
+Console.WriteLine("Число элементов больше 0 равно "+ bolshezero(M));
---
+++ Lesson6.HW43/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 43
+
+Console.WriteLine("Задача 43");
+Console.Write("Введите число b1 - ");
+float b1 = float.Parse(Console.ReadLine());
+Console.Write("Введите число k1 - ");
+float k1 = float.Parse(Console.ReadLine());
+Console.Write("Введите число b2 - ");
+float b2 = float.Parse(Console.ReadLine());
+Console.Write("Введите число k2 - ");
+float k2 = float.Parse(Console.ReadLine());
+
+List<double> per(float b1, float k1, float b2, float k2){
+    List<double> point = new List<double>();
+    point.Add((b2-b1)/(k1-k2));
+    point.Add(k1*point[0]+b1);
+    return point;
+}
+
+List<double> answer = per(b1,k1,b2,k2);
+
+Console.WriteLine("X:" + answer[0] + " Y:" + answer[1]);
+
